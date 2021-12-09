2b9817fa792960741bcfaf7a40e7dc1cf43d32a1
KiranSupekar <56031044+KiRaN1733@users.noreply.github.com>
2021-12-09 14:57:13 +0530

    Add files via upload

---
+++ 8thDecAssignments/4DisplayDigitsIndividually.cs
@@ -0,0 +1,40 @@
+﻿// Write a program in C# Sharp to display the individual digits of a given number using recursion. 
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes._8thDecAssignments
+{
+    class _4DisplayDigitsIndividually
+    {
+        static void displayDigits(int n)
+        {
+            if (n < 10)
+            {
+                Console.Write(" " + n);
+                return;
+            }
+            displayDigits(n / 10);
+            Console.Write(" " + n%10);
+            return;
+
+        }
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write(" Enter a number: ");
+                int n = int.Parse(Console.ReadLine());
+                Console.Write(" The digits in the number {0} are:",n);
+                displayDigits(n);
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(" Exception: " + ex.Message);
+            }
+
+        }
+    }
+}
