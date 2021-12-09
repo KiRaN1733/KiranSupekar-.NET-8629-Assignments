16e189adbbbaffdcd679b84d0e8cfd642000e65a
KiranSupekar <56031044+KiRaN1733@users.noreply.github.com>
2021-12-09 11:57:19 +0530

    Add files via upload

---
+++ 8thDecAssignments/2NaturalNumsFromNto1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes._8thDecAssignments
+{
+    class _2NaturalNumsFromNto1
+    {
+        static int PrintNums(int start, int n)
+        {
+            if (n < 1)
+            {
+                return start;
+            }
+
+            Console.Write(" {0} ", n);
+            n--;
+            return PrintNums(start, n);
+        }
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write(" Enter how many natural numbers to print: ");
+                int n = int.Parse(Console.ReadLine());
+                Console.Write(" Natural number from {0} to 1 are: ", n);
+                PrintNums(1, n);
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(" Exception: " + ex.Message);
+            }
+        }
+    }
+}
