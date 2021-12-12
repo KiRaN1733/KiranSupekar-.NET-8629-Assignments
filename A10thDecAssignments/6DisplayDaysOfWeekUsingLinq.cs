8dbee59d68bf308a2d3290d1edae31da5cc1d3ca
KiranSupekar <56031044+KiRaN1733@users.noreply.github.com>
2021-12-12 20:31:28 +0530

    Add files via upload

---
+++ A10thDecAssignments/6DisplayDaysOfWeekUsingLinq.cs
@@ -0,0 +1,26 @@
+﻿// Write a program in C# Sharp to display the name of the days of a week. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Classes._10thDecAssignments
+{
+    class _6DisplayDaysOfWeekUsingLinq
+    {
+        public static void Main(String[] args)
+        {
+            string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+
+            var weekDays = from Day in days select Day;
+
+            Console.WriteLine(" Days of the Week: ");
+            int i = 0;
+            foreach (var day in weekDays)
+            {
+                Console.WriteLine(" "+(++i)+") "+day);
+            }
+        }
+    }
+}
