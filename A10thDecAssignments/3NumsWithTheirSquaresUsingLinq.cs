e62029647e785276b8398eea60d33ca786ef3415
KiranSupekar <56031044+KiRaN1733@users.noreply.github.com>
2021-12-12 19:38:40 +0530

    Add files via upload

---
+++ A10thDecAssignments/3NumsWithTheirSquaresUsingLinq.cs
@@ -0,0 +1,24 @@
+﻿// Write a program in C# Sharp to find the number of an array and the square of each number. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Classes._10thDecAssignments
+{
+    class _3NumsWithTheirSquaresUsingLinq
+    {
+        public static void Main(string[] args)
+        {
+            var arr1 = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var sqNo = from int Number in arr1
+                       let SqrNo = Number * Number
+                       where SqrNo > 20
+                       select new { Number, SqrNo };
+
+            foreach (var a in sqNo)
+                Console.WriteLine(a);
+        }
+    }
+}
---
+++ A10thDecAssignments/4FreqOfNumsUsingLinq.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Classes._10thDecAssignments
+{
+    class _4FreqOfNumsUsingLinq
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 2, 3, 4, 1, 5, 2, 5, 1, 3, 6, 7, 4, 3, 3, 4, 1 };
+
+            var frequencies = from x in arr group x by x into y select y;
+
+            foreach(var frequency in frequencies)
+            {
+                Console.Write(" Number "+frequency.Key + " appears " + frequency.Count() + " times\n");
+            }
+        }
+    }
+}
