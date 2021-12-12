03b14d763d0853ec8090d83537d4bee2db78b8ce
KiranSupekar <56031044+KiRaN1733@users.noreply.github.com>
2021-12-12 21:37:53 +0530

    Add files via upload

---
+++ A10thDecAssignments/8CheckStringStartsWithSpecificCharUsingLinq.cs
@@ -0,0 +1,39 @@
+﻿// Write a program in C# Sharp to find the string which starts and ends with a specific character.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Classes._10thDecAssignments
+{
+    class _8CheckStringStartsWithSpecificCharUsingLinq
+    {
+        public static void Main(String[] args)
+        {
+            string[] cities = new string[] { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+            char ch;
+            string start, end;
+
+            try
+            {
+                Console.Write(" Enter Starting Character: ");
+                ch = Convert.ToChar(Console.ReadLine());
+                start = ch.ToString();
+
+                Console.Write(" Enter Ending Character: ");
+                ch = Convert.ToChar(Console.ReadLine());
+                end = ch.ToString();
+
+                var names = from city in cities where city.StartsWith(start) where city.EndsWith(end) select city;
+                foreach (var name in names)
+                {
+                    Console.WriteLine(" " + name);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(" Exception: " + ex.Message);
+            }
+        }
+    }
+}
